f65df9c3f7913257f294244b6e3ee252c4259223
alyohandrio <alexdanilyu@gmail.com>
2023-12-06 02:30:29 +0300

    Crap

--- download_model.sh
+++ download_model.sh
@@ -1,3 +1,3 @@
-gdown https://drive.google.com/uc?id=GXowccEkunVzLsfXuq8NqP9cIsfU-R1I
+gdown https://drive.google.com/uc?id=1GXowccEkunVzLsfXuq8NqP9cIsfU-R1I
 mkdir -p checkpoints
 mv checkpoing.pth checkpoints/model.pth
